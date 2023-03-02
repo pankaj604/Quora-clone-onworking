bc031e1f0102787daf473ebaeea51be391f910e7
DELL <pkewat604@gmail.com>
2023-03-02 21:21:09 +0530

    link add

--- src/components/SidebarOptions.js
+++ src/components/SidebarOptions.js
@@ -7,7 +7,7 @@ function SidebarOptions() {
     <div className="sidebarOptions">
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-930-100-cbbsbwijdhpyzlpipejvqpiijhhoaday.jpeg"
+          src="https://nct.ac.in/assets/img/history/slider/2.png"
           alt=""
         />
         <p>History</p>
@@ -15,7 +15,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-858-100-VnZbEVtOIGkEHXlnYId9slumV59IPgkA.jpeg"
+          src="https://images.livemint.com/img/2022/11/29/1600x900/1df36746-700f-11ed-9470-6981fab06e1e_1669744728301.jpg"
           alt=""
         />
 
@@ -23,7 +23,7 @@ function SidebarOptions() {
       </div>
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-1913-100-B8JrwaVauFzsaTSqXDqoWLCXzQb2mTE9.jpeg"
+          src="https://assets.ltkcontent.com/images/44558/using-psychology_15cb79a465.jpg"
           alt=""
         />
         <p>Psychology</p>
@@ -31,7 +31,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-877-100-e7jKHEQr0HExAIA9rlsyHlV6HJyRruEo.jpeg"
+          src="https://www.thedailymeal.com/img/gallery/basic-cooking-methods-you-need-to-know/Basic-Cooking-Methods.jpg"
           alt=""
         />
         <p>Cooking</p>
@@ -39,7 +39,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-801-100-Sf8h894FXbQZQit0TeqDrrqS6xw6dwCQ.jpeg"
+          src="https://img.freepik.com/free-vector/musical-pentagram-sound-waves-notes-background_1017-33911.jpg?w=2000"
           alt=""
         />
         <p>Music</p>
@@ -47,7 +47,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-931-100-c8WCPwZ9qPsh5zLGQ5wHh1ddxtc9Cch7.jpeg"
+          src="https://static.theprint.in/wp-content/uploads/2019/11/science.jpg"
           alt=""
         />
         <p>Science</p>
@@ -55,7 +55,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-1140-100-24q3tiv4WhPssc5TGwf0mvCM5aiqGVXW.jpeg"
+          src="https://www.healthkart.com/connect/wp-content/uploads/2021/09/900x500_banner_HK-Connect_How-to-Improve-Heart-Health-_-Points-To-Keep-In-Mind.jpg"
           alt=""
         />
         <p>Health</p>
@@ -63,7 +63,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-843-100-W7FzODceTO2aQmp8D7E4rKZ8YgSv21eR.jpeg"
+          src="https://images.news18.com/ibnkhabar/uploads/2022/12/TOP-10-Movies-of-2022.jpg?impolicy=website&width=904&height=null"
           alt=""
         />
         <p>Movies</p>
@@ -71,7 +71,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-2177-100-JiR07D1TQSfeQzRvWXomVaY4Poj2f8Yb.jpeg"
+          src="https://www.universal-rights.org/wp-content/uploads/2019/09/30212411048_2a1d7200e2_z-1.jpg"
           alt=""
         />
         <p>Technology</p>
@@ -79,7 +79,7 @@ function SidebarOptions() {
 
       <div className="sidebarOption">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-t-996-100-bfZBQjeEenKKl8fcNY4tVv0FyArtB0Mb.jpeg"
+          src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop  "
           alt=""
         />
         <p>Education</p>
--- src/components/WidgetContent.js
+++ src/components/WidgetContent.js
@@ -6,7 +6,7 @@ function WidgetContent() {
     <div className="widget__contents">
       <div className="widget__content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1737435-100-jxcfmjdvwvpkcketifttdmeeimxcatua.jpeg"
+          src="https://reliasoftware.com/static/9ce8aa204a21e69716018fee472b042c/the-complete-guide-to-mobile-app-development-2021.png"
           alt=""
         />
         <div className="widget__contentTitle">
@@ -16,7 +16,7 @@ function WidgetContent() {
       </div>
       <div className="widget__content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1574818-100-mzdwostcualpwcxekyrvyqqpychetdoc.jpeg"
+          src="https://cdn.quotesgram.com/img/50/14/1373797318-quote-not-one-has-shown-an-iota-of-fear-of-death-they-want-to-end-this-agony-jack-kevorkian-101340.jpg"
           alt=""
         />
         <div className="widget__contentTitle">
@@ -26,7 +26,7 @@ function WidgetContent() {
       </div>
       <div className="widget__content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1644613-100-ydflucgoeztbhwyurtmlqqrgfqmjmhpl.jpeg"
+          src="https://i0.wp.com/xaviernewswire.com/wp-content/uploads/2020/02/art.jpg?fit=948%2C572&ssl=1"
           alt=""
         />
         <div className="widget__contentTitle">
@@ -36,7 +36,7 @@ function WidgetContent() {
       </div>
       <div className="widget__content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1647318-100-kmwvqbpzatmylibelrowrerfqbspekwo.jpeg"
+          src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-515350818jpg-.jpg?resize=1200:*"
           alt=""
         />
         <div className="widget__contentTitle">
@@ -46,7 +46,7 @@ function WidgetContent() {
       </div>
       <div className="widget__content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1578647-100-xkggvbyzfkvzhyklewtkjijefekqbazb.jpeg"
+          src="https://a.c-dn.net/c/content/igcom/en_AU/ig-financial-markets/market-news-and-analysis/trading-strategies/2021/05/10/top-7-price-action-trading-strategies/jcr:content/ArticleContent/image_163386472.adaptive.620.high.jpg/1620721086536.jpg"
           alt=""
         />
         <div className="widget__contentTitle">
@@ -56,7 +56,7 @@ function WidgetContent() {
       </div>
       <div className="widget__content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1711686-100-glvgnbatdxpjbhrkyphlfamqrryfccvp.jpeg"
+          src="https://images.adsttc.com/media/images/5bd8/453a/f197/cc17/2700/02c9/newsletter/PORTADA_The_Interlace_by_OMA_Ole_Scheeren_photo_--%C2%BC_Iwan_Baan_01.jpg?1540900147"
           alt=""
         />
         <div className="widget__contentTitle">
